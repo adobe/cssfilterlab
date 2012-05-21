f82517fe077b9f1e94baa0f48b47032dfc3971b4
Max Vujovic <mvujovic@adobe.com>
2012-05-21 16:12:58 -0700

    Updated rolling-scroll to use blend shaders. Back color needs gl_FragColor or  another blend mode to be implemented properly, though. Right now, it's just blended with the texture instead of replacing it.

--- shaders/fragment/rolling-scroll.fs
+++ shaders/fragment/rolling-scroll.fs
@@ -1,31 +1,23 @@
 /*
 Copyright 2011 Adobe Systems, incorporated
-This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ . 
+This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ .
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
 
 precision mediump float;
-varying vec2 v_texCoord;
+
 varying float v_lighting;
-uniform sampler2D u_texture;
-float PI = 3.1415;
+
 uniform float t;
-
 uniform vec4 backColor;
-
 uniform float useColoredBack;
 
 void main()
 {
-    vec4 color;
-    if (gl_FrontFacing || useColoredBack < 0.5) {
-        color = texture2D(u_texture, v_texCoord);
-        if (color.w < 0.001)
-            discard;
-    } else {
-        color = backColor;
-    }
-    color = vec4((color.xyz * v_lighting), color.w);
-    color = clamp(color, 0.0, 1.0);
-    gl_FragColor = color;
+    // TODO: When we have more blend modes, we can have the backColor replace the texture color.
+    // Or, we can enable gl_FragColor and define its interaction with css_BlendColor.
+    if (!gl_FrontFacing && useColoredBack >= 0.5)
+        css_BlendColor = backColor;
+
+    css_BlendColor *= vec4(v_lighting, v_lighting, v_lighting, 1.0);
 }
