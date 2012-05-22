802eac6bd73e752483420747b64bc1f0610e7ce0
Max Vujovic <mvujovic@adobe.com>
2012-05-22 08:29:50 -0700

    Updated grayscale to use blend shaders.

--- shaders/fragment/grayscale.fs
+++ shaders/fragment/grayscale.fs
@@ -1,32 +1,19 @@
 /*
-Copyright 2011 Adobe Systems, incorporated
-This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ . 
+Copyright 2012 Adobe Systems, Incorporated
+This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ .
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
 
 precision mediump float;
-uniform sampler2D u_texture;
+
+// This uniform values is passed in using CSS.
 uniform float amount;
 
-varying vec2 v_texCoord;
-
-mat3 colorMatrix = mat3(
-    vec3(0.2127, 0.2127, 0.2127),
-    vec3(0.7152, 0.7152, 0.7152),
-    vec3(0.0722, 0.0722, 0.0722)
-);
-
-
-void main() {
-    vec4 color = texture2D(u_texture, v_texCoord);
-    vec4 gray = vec4(color);
-
-    if (gray.a > 0.0) {
-        gray.rgb /= gray.a;
-    }
-    
-    gray.rgb = colorMatrix * color.rgb;
-    color.rgb *= color.a;
-    
-    gl_FragColor = (1.0 - amount) * color + amount * gray;
+void main()
+{
+    float oneMinusAmount = 1.0 - amount;
+    css_ColorMatrix = mat4((0.2126 + 0.7874 * oneMinusAmount), (0.7152 - 0.7152 * oneMinusAmount), (0.0722 - 0.0722 * oneMinusAmount), 0.0,
+                           (0.2126 - 0.2126 * oneMinusAmount), (0.7152 + 0.2848 * oneMinusAmount), (0.0722 - 0.0722 * oneMinusAmount), 0.0,
+                           (0.2126 - 0.2126 * oneMinusAmount), (0.7152 - 0.7152 * oneMinusAmount), (0.0722 + 0.9278 * oneMinusAmount), 0.0,
+                           0.0, 0.0, 0.0, 1.0);
 }
