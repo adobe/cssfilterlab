20d211c84ee1f111921208fd1b04ab59e071af13
Max Vujovic <mvujovic@adobe.com>
2012-05-24 15:02:19 -0700

    Added transpose workaround in grayscale for current version of Chromium prototype.

--- shaders/fragment/grayscale.fs
+++ shaders/fragment/grayscale.fs
@@ -8,6 +8,16 @@ precision mediump float;
 
 uniform float amount;
 
+mat4 transposeMatrix(mat4 m)
+{
+    return mat4(
+        m[0].x, m[1].x, m[2].x, m[3].x,
+        m[0].y, m[1].y, m[2].y, m[3].y,
+        m[0].z, m[1].z, m[2].z, m[3].z,
+        m[0].w, m[1].w, m[2].w, m[3].w
+    );
+}
+
 void main()
 {
     const vec3 grayFactor = vec3(0.2127, 0.7152, 0.0722);
@@ -16,11 +26,13 @@ void main()
     vec3 m0 = mix(vec3(0.0), grayFactor, amount);
 
     mat4 colorMatrix = mat4(
-        vec4(m1.r, m0.g, m0.b, 0.0),
-        vec4(m0.r, m1.g, m0.b, 0.0),
-        vec4(m0.r, m0.g, m1.b, 0.0),
-        vec4(0.0, 0.0, 0.0, 1.0)
+        m1.r, m0.r, m0.r, 0.0,
+        m0.g, m1.g, m0.g, 0.0,
+        m0.b, m0.b, m1.b, 0.0,
+        0.0, 0.0, 0.0, 1.0
     );
 
-    css_ColorMatrix = colorMatrix;
+    // TODO: Fix.
+    //css_ColorMatrix = colorMatrix;
+    css_ColorMatrix = transposeMatrix(colorMatrix);
 }
