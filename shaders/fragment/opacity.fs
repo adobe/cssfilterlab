f6af2973877b284b71a28a08961528e3dd72566b
Max Vujovic <mvujovic@adobe.com>
2012-05-24 16:35:14 -0700

    Updated opacity.fs to use blend shaders.

--- shaders/fragment/opacity.fs
+++ shaders/fragment/opacity.fs
@@ -1,23 +1,14 @@
 /*
 Copyright 2011 Adobe Systems, incorporated
-This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ . 
+This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ .
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
 
 precision mediump float;
-uniform sampler2D u_texture;
+
 uniform float amount;
 
-varying vec2 v_texCoord;
-
-void main() {
-    vec4 color = texture2D(u_texture, v_texCoord);
-    if (color.a > 0.0) {
-        color.rgb /= color.a;
-    }
-    
-    color.a *= (1.0 - amount);
-    color.rgb *= color.a;
-    
-    gl_FragColor = color;
+void main()
+{
+    css_BlendColor = vec4(1.0 - amount);
 }
