17f7738bcf317cafbf27b8cc4227cb3b3f9dc030
Alexandru Chiculita <achicu@adobe.com>
2012-08-12 20:23:17 -0700

    Adding edited shader support

--- shaders/fragment/fold.fs
+++ shaders/fragment/fold.fs
@@ -1,5 +1,5 @@
 /*
-Copyright 2011 Adobe Systems, incorporated
+Copyright 2012 Adobe Systems, Incorporated
 This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ .
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
@@ -8,16 +8,12 @@ precision mediump float;
 
 varying float v_lighting;
 
+// This uniform value is passed in using CSS.
 uniform float t;
-uniform vec4 backColor;
-uniform float useColoredBack;
+
+const float PI = 3.1415;
 
 void main()
 {
-    // TODO: When we have more blend modes, we can have the backColor replace the texture color.
-    // Or, we can enable gl_FragColor and define its interaction with css_BlendColor.
-    if (!gl_FrontFacing && useColoredBack >= 0.5)
-        css_BlendColor = backColor;
-
-    css_BlendColor *= vec4(v_lighting, v_lighting, v_lighting, 1.0);
+    css_BlendColor = vec4(vec3(v_lighting), 1.0);
 }
--- shaders/fragment/warp.fs
+++ shaders/fragment/warp.fs
@@ -1,28 +1,19 @@
 /*
-Copyright 2011 Adobe Systems, incorporated
-This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ . 
+Copyright 2012 Adobe Systems, Incorporated
+This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License http://creativecommons.org/licenses/by-nc-sa/3.0/ .
 Permissions beyond the scope of this license, pertaining to the examples of code included within this work are available at Adobe http://www.adobe.com/communities/guidelines/ccplus/commercialcode_plus_permission.html .
 */
 
 precision mediump float;
-varying vec2 v_texCoord;
-uniform sampler2D u_texture;
-uniform vec4 backColor;
 
-uniform float backVigneteAmount;
-uniform float backVigneteSize;
-uniform float useColoredBack;
+varying float v_lighting;
+
+// This uniform value is passed in using CSS.
+uniform float t;
+
+const float PI = 3.1415;
 
 void main()
 {
-    vec4 color;
-    if (gl_FrontFacing || useColoredBack < 0.5) {
-        color = texture2D(u_texture, v_texCoord);
-        if (color.w < 0.001)
-            discard;
-    } else {
-        color = backColor;
-    }
-
-    gl_FragColor = color;
+    css_BlendColor = vec4(vec3(v_lighting), 1.0);
 }
